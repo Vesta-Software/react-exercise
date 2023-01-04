437cb72bbe8a82ac9792a98a96031f9c8a6c9856
Brian Issleb <brian@getvesta.io>
2023-01-03 21:09:24 -0600

    App shell.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { AppShell, Navbar, Header, Image, NavLink } from "@mantine/core";
+import logo from "./logo.png";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppShell
+      padding="md"
+      navbar={
+        <Navbar width={{ base: 300 }} height={500} p="xs">
+          <NavLink
+            label="Properties"
+            color={"violet"}
+            variant="filled"
+            r
+            active
+          />
+        </Navbar>
+      }
+      header={
+        <Header height={50} p="xs">
+          <Image width={100} height={26} src={logo} />
+        </Header>
+      }
+    >
+      {/* Your application here */}
+    </AppShell>
   );
 }
 
